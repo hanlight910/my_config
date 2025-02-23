f60c78c39f91736d9757c1c01c49047fc49eacc6
hanlight910 <leehanvit511@gamil.com>
2025-02-24 03:41:09 +0900

    chore: Replace desktop file copy with rsync in desktops.sh and add ollama script

--- bash_config/install/desktops.sh
+++ bash_config/install/desktops.sh
@@ -1,5 +1,7 @@
 mkdir -p ~/.config/autostart 
-for file in $BASH_CONFIG/desktops/*; do 
-	echo $file
-	cp "$file" "$HOME/.config/autostart/${file##*/}"; 
-done
+# for file in $BASH_CONFIG/desktops/*; do 
+# 	echo $file
+# 	cp "$file" "$HOME/.config/autostart/${file##*/}"; 
+# done
+
+rsync -avd --delete $BASH_CONFIG/desktops/ ~/.config/autostart/
