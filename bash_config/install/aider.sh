8010704b31bf0c6303253766fd1143dc4392bb59
hanlight910 <leehanvit511@gamil.com>
2025-02-24 23:37:51 +0900

    chore: Update installation scripts and permissions for various tools

--- bash_config/install/aider.sh
+++ bash_config/install/aider.sh
@@ -1,3 +1,4 @@
 curl -LsSf https://aider.chat/install.sh | sh
 
-aider --model o1-mini --api-key openai=$OPENAI_API_KEY
+# aider --model o1-mini --api-key openai=$OPENAI_API_KEY
+
--- bash_config/install/kanata.sh
+++ bash_config/install/kanata.sh
@@ -1,4 +1,9 @@
 cargo install kanata
 
 mkdir ~/.config/systemd/user/ -p
+
+# user wide
 cp $CONFIG/kanata/kanata.service ~/.config/systemd/user/
+
+# system-wide during booting process to active during login session
+sudo cp $CONFIG/kanata/kanata.service /etc/systemd/system/kanata.service
