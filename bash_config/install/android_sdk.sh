aee34d3b9ce843908580e4803c2238d8561121de
hanlight910 <leehanvit511@gamil.com>
2025-03-08 02:14:06 +0900

    feat installed android_sdk

---
+++ bash_config/install/android_sdk.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://developer.android.com/studio
+
+mkdir -p ~/Library/Android/sdk
+unzip commandlinetools-linux-*.zip -d ~/Library/Android/sdk/tools/
+rm commandlinetools-linux-*.zip
---
+++ bash_config/scripts/backup_gnomesetting.sh
@@ -0,0 +1,2 @@
+dconf dump / > gnome-settings-backup.ini
+mv gnome-settings-backup.ini $BASH_CONFIG/gnome_setting/
