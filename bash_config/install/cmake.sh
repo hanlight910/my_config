23fa7c8fd78dbaa8c744c2e6caa6c1de5f3dff1a
hanlight910 <leehanvit511@gamil.com>
2025-03-09 13:08:43 +0900

    update

---
+++ bash_config/install/cmake.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://cmake.org/download/
+
+# sudo apt install build-essential -y
+# in ubuntu gmake is actually a make command
+
+tar -zxvf cmake-*.tar.gz
--- bash_config/install/mail.sh
+++ bash_config/install/mail.sh
@@ -1,2 +1,3 @@
+# ubuntu apt
 sudo apt install mailutils -y
 # no configuration
