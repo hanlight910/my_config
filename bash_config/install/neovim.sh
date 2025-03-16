ea3a30fe9d2ce335fb138b900c8923f69d53bff6
hanlight910 <leehanvit511@gamil.com>
2025-03-16 18:29:41 +0900

    add install nvim packer

--- bash_config/install/neovim.sh
+++ bash_config/install/neovim.sh
@@ -4,3 +4,5 @@ sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
